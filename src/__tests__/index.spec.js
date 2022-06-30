2388aec61b022dbaf27e7a6225c8e6261d4f766d
rammfall <15bogdan17@gmail.com>
2022-06-30 23:40:46 +0300

    Show env variables

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -2,6 +2,7 @@ import fastify from '../index';
 
 describe('fastify', () => {
   it('should returns correct string', async () => {
+    console.log(process.env);
     const result = await fastify.inject({
       method: 'GET',
       url: '/hello',
